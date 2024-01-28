a158fa7145a86304c65cc86b5db2a2bd9a6c3ec4
SHERCAN <sidereoyess@gmail.com>
2024-01-27 20:41:29 -0500

    Docker fl0 test

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Ruta principal que muestra "Hola, mundo!"
+app.get("/", (req, res) => {
+  res.send("Hola, mundo!");
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
